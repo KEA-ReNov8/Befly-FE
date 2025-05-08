483f4d8a0dadcb2940547e7a0e2c0892c19fbaa7
Dongyun Kim <djskaldrjfj@naver.com>
2025-05-08 12:09:39 +0900

    KAN-138/feat/디자인시스템 추가

---
+++ src/app/styles/fonts.js
@@ -0,0 +1,42 @@
+export const fontFamily = {
+    pretendard: "Pretendard",
+};
+
+export const fontSize = {
+    xsmall: "0.8rem",
+    small: "1.2rem",
+    smallMedium: "1.4rem",
+    medium: "1.6rem",
+    large: "2rem",
+    xlarge: "2.4rem",
+    title: "4rem",
+};
+
+export const font = {
+    regular: {
+        fontWeight: 400,
+    },
+    medium: {
+        fontWeight: 500,
+    },
+    semibold: {
+        fontWeight: 600,
+    },
+    bold: {
+        fontWeight: 700,
+    },
+};
+
+//상황에 따라 아래의 방식을 활용할 수 있음 -> 현재는 theme 파일에 직접 폰트 파일 받아 사용
+/*const createFontStyle = (family: string, weight: number, size: number, lineHeight: number) => `
+  font-family: "${family}";
+  font-weight: ${weight};
+  font-size: ${size}px;
+  line-height: ${lineHeight}px;
+  letter-spacing: 0%;
+`;
+export const fonts: FontsTypes = {
+  heading_bold_24px: createFontStyle('Pretendard', 700, 24, 34),
+  heading_medium_20px: createFontStyle('Pretendard', 500, 20, 34),
+  body_bold_16px: createFontStyle('Pretendard', 700, 16, 24),
+};*/
