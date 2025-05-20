760adeea9bf1ec5fc9438331ae0a10a46627b601
Dongyun Kim <djskaldrjfj@naver.com>
2025-05-21 04:02:55 +0900

    KAN-174/feat/공유게시글 카드 컴포넌트 구현

---
+++ src/pages/my/components/SharePostCard.jsx
@@ -0,0 +1,99 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import theme from '@app/styles/theme';
+
+const SharePostCard = ( { post } ) => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/shared-post/${post.id}`);
+    };
+
+    return (
+        <PostCardContainer onClick={handleClick}>
+            <Category data-category={post.category}>{post.category}</Category>
+            <Title>{post.title}</Title>
+            <CardFooter>
+                <PostDate>{post.date}</PostDate>
+                <PostStat>
+                <span>♡&nbsp;{post.likes}</span>
+                <span>💬&nbsp;{post.comments}</span>
+                </PostStat>
+            </CardFooter>
+        </PostCardContainer>
+    );
+};
+
+const PostCardContainer = styled.div`
+    width: 230px;
+    height: 160px;
+    border-radius: 5px;
+    border: 1px solid ${theme.colors.gray[400]};
+    background-color: ${theme.colors.white};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: all 0.3s;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    
+    &:hover {
+        border: 1px solid ${theme.colors.gray[300]};
+        box-shadow: 0px 0px 10px 0px rgba(18, 165, 27, 0.2);
+        transform: scale(1.01);
+`;
+
+const Category = styled.p`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    border-bottom: 1px solid ${theme.colors.gray[200]};
+    border-radius: 10px;
+    width: 66px;
+    height: 28px;
+    margin-bottom: 20px;
+    background-color: ${(props) => theme.colors.category[props['data-category']]};
+    color: ${theme.colors.other.white};
+    font-family: ${theme.fontFamily.pretendard};
+`;
+
+const Title = styled.p`
+    text-align: center;
+    width: 200px;
+    font-size: 17px;
+    font-family: ${theme.fontFamily.pretendard};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    padding: 10px;
+`;
+
+const CardFooter = styled.div`
+    display:flex;
+    align-items: center;
+    font-family: ${theme.fontFamily.pretendard};
+    margin-top: 20px;
+    border-top: 1px solid ${theme.colors.gray[200]};
+    padding-top: 10px;
+    width: 100%;
+`;
+
+const PostDate = styled.p`
+    margin-left: 20px;
+    font-size: ${theme.fontSize.xsmall};
+    font-family: ${theme.fontFamily.pretendard};
+`;
+
+const PostStat = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    margin-left: 60px;
+    font-size: ${theme.fontSize.xsmall};
+`;
+
+export default SharePostCard;
