be501706aa54e1f5ef8908a9314e44a211fb7d80
Dongyun Kim <djskaldrjfj@naver.com>
2025-05-21 04:05:31 +0900

    KAN-174/mod/my index 수정

--- src/pages/my/index.js
+++ src/pages/my/index.js
@@ -1,4 +1,2 @@
 export { MyPage } from '@my/ui/MyPage';
-export { MyFreePage } from '@my/ui/MyFreePage';
-export { MySharePage } from '@my/ui/MySharePage';
 export { MyWorryPage } from '@my/ui/MyWorryPage';
