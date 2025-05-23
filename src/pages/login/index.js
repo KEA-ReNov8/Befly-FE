e2b3e51565efe9a857196d35cbb0a9e4096e3fcc
Dongyun Kim <djskaldrjfj@naver.com>
2025-05-24 02:41:05 +0900

    KAN-175/mod/login index 수정

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,2 +1,4 @@
 export { LoginPage } from '@login/ui/LoginPage';
 export { SignupPage } from '@login/ui/SignupPage';
+export { SelfSignupPage } from '@login/ui/SelfSignupPage';
+export { FindPwPage } from '@login/ui/FindPwPage';
