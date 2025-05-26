84de2d2176c38a563d0df4f753544b72a0df057d
Dongyun Kim <djskaldrjfj@naver.com>
2025-05-26 12:35:26 +0900

    KAN175-2/refector/알림 디자인 수정

--- src/shared/ui/TopBar/NotificationItem.jsx
+++ src/shared/ui/TopBar/NotificationItem.jsx
@@ -15,10 +15,10 @@ const ItemWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px 0;
-  font-size: 12px;
-  color: ${theme.colors.gray[600]};
-  border-bottom: 1px solid ${theme.colors.gray[200]};
+  padding:15px 0;
+  font-size: ${theme.fontSize.smMd};
+  color: ${theme.colors.gray[800]};
+  border-bottom: 1px solid ${theme.colors.gray[300]};
 
   &:last-child {
     border-bottom: none;
@@ -31,7 +31,7 @@ const Message = styled.span`
 
 const Time = styled.span`
   flex-shrink: 0;
-  font-size: 11px;
-  color: ${theme.colors.gray[500]};
+  font-size: ${theme.fontSize.sm};
+  color: ${theme.colors.gray[600]};
   margin-left: 10px;
 `;
