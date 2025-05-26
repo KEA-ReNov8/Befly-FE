4270aa5423d0ef3f5f5dd651b72150855153022b
Dongyun Kim <djskaldrjfj@naver.com>
2025-05-26 12:40:21 +0900

    KAN-175-2/refactor/유저페이지 디자인 수정

--- src/pages/profile/ui/UserProfilePage.jsx
+++ src/pages/profile/ui/UserProfilePage.jsx
@@ -20,18 +20,16 @@ const Wrapper = styled.div`
     align-items: center;
     width: 1440px;
     margin: 0 auto;
-    background-color: ${theme.colors.gray[100]};
 `;
 
 const Line = styled.div`
     width: 100%;
     height: 66px;
     background-color: ${theme.colors.green.main};
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
     display: flex;
     align-items: center;
-    font-size: 25px;
-    font-weight: 600;
+    font-size: ${theme.fontSize.xl};
+    font-weight: ${theme.fontWeight.bold};
     padding-left: 220px;
     color: ${theme.colors.other.white};
 `;
