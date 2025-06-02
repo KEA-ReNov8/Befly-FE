a7d208d1c0fd6638c7fdcbb331e4bf6bd0777711
Dongyun Kim <djskaldrjfj@naver.com>
2025-06-03 07:53:45 +0900

    KAN-179/feat/로그인 상태 전역 저장소 추가

---
+++ src/shared/store/useIsLoggedInStore.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+const useIsLoggedInStore = create(
+    persist(
+        (set) => ({
+            isFirstMount: true,
+            isLoggedIn: false,
+            setIsFirstMount: (isFirstMount) => set({ isFirstMount }),
+            setIsLoggedIn: (isLoggedIn) => set({ isLoggedIn }),
+            logout: () => {
+                set({ isLoggedIn: false, isFirstMount: true});
+                localStorage.removeItem('is-logged-in-store');
+            }
+        }),
+        {
+            name: 'is-logged-in-store',
+            storage: createJSONStorage(() => localStorage), //또는 sessionStorage
+        },
+    ),
+);
+
+export default useIsLoggedInStore;
