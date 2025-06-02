a4bc3d2b106856e499af993f9a4cbcb44096c87c
Dongyun Kim <djskaldrjfj@naver.com>
2025-06-03 08:21:00 +0900

    KAN-179-1/chore/파일 정리

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -1,6 +1,6 @@
 export { FreePage } from '@post/ui/FreePage';
 export { FreePostListPage } from '@post/ui/FreePostListPage';
-export { FreePostPage } from '@/pages/post/ui/FreePostPage';
+export { FreePostPage } from '@post/ui/FreePostPage';
 export { SharePage } from '@post/ui/SharePage';
 export { SharePostListPage } from '@post/ui/SharePostListPage';
 export { SharePostPage } from '@post/ui/SharePostPage';
