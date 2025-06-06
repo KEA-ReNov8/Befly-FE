9cecd74be4ec7de03e982d08dfa7e0663f2020ad
Dongyun Kim <djskaldrjfj@naver.com>
2025-06-06 17:33:59 +0900

    KAN-182/mod/제안 api 연결

--- src/pages/report/components/Advise.jsx
+++ src/pages/report/components/Advise.jsx
@@ -5,13 +5,8 @@ const Advise = ({ adviseItems }) => {
   return (
     <AdviseContainer>
       <Title>나래의 제안</Title>
-      {adviseItems && adviseItems.length > 0 ? (
-        adviseItems.map((item, index) => (
-          <AdviseItem key={index}>
-            <AdviseTitle>{item.title}</AdviseTitle>
-            <AdviseContent>{item.content}</AdviseContent>
-          </AdviseItem>
-        ))
+      {adviseItems ? (
+        <AdviseContent>{adviseItems}</AdviseContent>
       ) : (
         <p>조언 데이터를 불러오는 중입니다...</p>
       )}
@@ -27,6 +22,13 @@ const AdviseContainer = styled.div`
   border-radius: 8px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
   margin-bottom: 20px;
+  transition: scale 0.3s ease;
+
+  &:hover {
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+    transition: scale 0.3s ease;
+    scale: 1.01;
+  }
 `;
 
 const Title = styled.h2`
@@ -36,26 +38,12 @@ const Title = styled.h2`
   margin-bottom: 20px;
 `;
 
-const AdviseItem = styled.div`
-  margin-bottom: 15px;
-  padding: 16px;
-  //background-color: ${theme.colors.other.white};
-  //border-left: 4px solid ${theme.colors.green.main};
-  border-radius: 8px;
-`;
-
-const AdviseTitle = styled.h3`
-  font-weight: ${theme.fontWeight.medium};
-  font-size: ${theme.fontSize.lgMd};
-  color: ${theme.colors.other.white};
-  margin-bottom: 8px;
-`;
-
 const AdviseContent = styled.p`
   font-weight: ${theme.fontWeight.regular};
   font-size: ${theme.fontSize.md};
   color: ${theme.colors.other.white};
   line-height: 1.8;
+  white-space: pre-wrap;
 `;
 
 export default Advise;
