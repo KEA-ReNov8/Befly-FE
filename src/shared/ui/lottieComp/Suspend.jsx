851e7ba1d1c41f62f24a7696c1997433e7c5c994
Dongyun Kim <djskaldrjfj@naver.com>
2025-06-07 17:49:26 +0900

    KAN-183/feat/중단  로티 추가

---
+++ src/shared/ui/lottieComp/Suspend.jsx
@@ -0,0 +1,8 @@
+import Lottie from 'lottie-react';
+import suspend from '@shared/assets/lotties/sad.json';
+
+const Suspend = () => {
+    return <Lottie animationData={suspend} loop={true} />;
+};
+
+export default Suspend;
