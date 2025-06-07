38b42e3c6c6d3a4542ed5323239073895a3d80ae
Dongyun Kim <djskaldrjfj@naver.com>
2025-06-07 17:45:32 +0900

    KAN-183/mod/유저 정보 api 수정

--- src/shared/apis/user/user.js
+++ src/shared/apis/user/user.js
@@ -3,7 +3,6 @@ import { apiInstance } from '@shared/apis/instance'
 export const fetchMyInfo = async () => {
     try {
         console.log('/user/profile 요청 전송 중...');
-        console.log('현재 쿠키:', document.cookie);
         const response = await apiInstance.get('/user/profile');
         console.log('/user/profile 응답:', response);
         console.log('응답 데이터:', response.data);
@@ -14,4 +13,4 @@ export const fetchMyInfo = async () => {
         console.error('에러 상태:', error.response?.status);
         throw error; // 에러를 다시 throw해서 호출하는 곳에서 catch할 수 있도록
     }
-}
+};
