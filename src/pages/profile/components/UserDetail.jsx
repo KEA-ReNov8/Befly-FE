add6a2f90f35e0457a0b898e0cd6b9054538a4bf
Dongyun Kim <djskaldrjfj@naver.com>
2025-06-07 21:19:52 +0900

    KAN-183/feat/타유저 프로필 api 연결

--- src/pages/profile/components/UserDetail.jsx
+++ src/pages/profile/components/UserDetail.jsx
@@ -1,14 +1,31 @@
 import styled from 'styled-components';
 import theme from '@app/styles/theme';
+import defaultProfile from '@shared/assets/icons/defaultUser.svg';
+import { badgeSystem } from '@my/util/badgeSystem';
 
-const UserDetail = ({ userData }) => {
+const UserDetail = ({userData}) => {
+    console.log('UserDetail userData:', userData);
+    
+    // userData가 없으면 로딩 상태 표시
+    if (!userData) {
+        return (
+            <ProfileSection>
+                <div>사용자 정보를 불러오는 중...</div>
+            </ProfileSection>
+        );
+    }
+    
+    const isDefaultProfile = !userData?.profileImg;
+    
     return (
         <ProfileSection>
-            <ProfileImage />
-                <NameSection>
-                    <UserName>{userData.username}</UserName>
-                    <Badge />
-                </NameSection>
+            <ProfileImage $isDefault={isDefaultProfile}>
+                <img src={userData?.profileImg || defaultProfile} alt="profile" />
+            </ProfileImage>
+            <NameSection>
+                <UserName>{userData?.nickName}</UserName>
+                <Badge src={badgeSystem(userData?.badge)} alt="badge" />
+            </NameSection>
         </ProfileSection>
     );
 };
@@ -19,15 +36,27 @@ const ProfileSection = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    cursor: pointer;
 `;
 
-const ProfileImage = styled.img`
+const ProfileImage = styled.div`
     width: 170px;
     height: 170px;
     border-radius: 50%;
     object-fit: cover;
     border: none;
-    background-color: ${theme.colors.gray[300]};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${theme.colors.gray[200]};
+    object-fit: cover;
+
+    img {
+        width: ${props => props.$isDefault ? '80px' : '170px'};
+        height: ${props => props.$isDefault ? '80px' : '170px'};
+        border-radius: ${props => props.$isDefault ? '0' : '50%'};
+        object-fit: ${props => props.$isDefault ? 'contain' : 'cover'};
+    }
 `;
 
 const NameSection = styled.div`
@@ -35,18 +64,18 @@ const NameSection = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 10px;
+    gap: 15px;
 `;
 
 const UserName = styled.p`
     margin-left: 30px;
     font-size: ${theme.fontSize.xl};
-    font-weight: ${theme.fontWeight.semibold};
+    font-weight: ${theme.fontWeight.bold};
 `;
 
 const Badge = styled.img`
-    width: 18px;
-    height: 18px;
+    width: 25px;
+    height: 25px;
 `;
 
 export default UserDetail;
